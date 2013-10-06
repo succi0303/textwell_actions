17684ac082658b7795858ef9921f7ea3d5b9b8f3
succi0303 <succi0303@gmail.com>
2013-10-06 23:16:11 +0900

    「gmailでメールを送る」を作成しました

---
+++ send_mail_with_gmail.js
@@ -0,0 +1,8 @@
+// Gmailでメールを送る
+// 1行目をSubject、2行目以降を本文とする
+var subject = T.line(1);
+var body = T.lines(2, null);
+T( 'urlScheme', {
+  url: 'googlegmail:///co?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(body),
+  option: 'none'
+});
