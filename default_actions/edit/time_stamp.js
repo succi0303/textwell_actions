6d056cfab7b73eda3b0d03d003493876dc4225e0
succi0303 <succi0303@gmail.com>
2013-10-13 01:20:44 +0900

    time stampを追加しました”

---
+++ default_actions/edit/time_stamp.js
@@ -0,0 +1,9 @@
+// Inserts time stamp at the current caret position.
+T( 'timeStamp', {
+  dateFormat: 'medium',
+  // 'none'|'short'|'medium'|'long'|'full'
+  timeFormat: 'medium',
+  // 'none'|'short'|'medium'|'long'|'full'
+  prefix: '',
+  suffix: ''
+});
