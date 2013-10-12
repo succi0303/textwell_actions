703a75603fb9488dfe1ea8d49353d1cd0bd7fbba
succi0303 <succi0303@gmail.com>
2013-10-13 01:09:57 +0900

    Location Stampを追加しました

---
+++ default_actions/edit/location_stamp.js
@@ -0,0 +1,9 @@
+// Inserts a location stamp at the current caret position.
+// Location stamp represents your currents your current geographical location.
+T( 'locationStamp', {
+  format: 'address',
+  // 'address'|'city'|'coordinates'|'addressAndCoordinates'|'coordinatesWithLabel'|'coordinatesAsLink'|'addressAndCoordinatesAsLink'
+  noLineBreak: false,
+  prefix: '',
+  suffix: ''
+});
