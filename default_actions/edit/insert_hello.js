8da8614e643e5e628f58e6f46b69541c10c7328d
succi0303 <succi0303@gmail.com>
2013-10-13 00:54:10 +0900

    insert helloを追加しました

---
+++ default_actions/edit/insert_hello.js
@@ -0,0 +1,5 @@
+// Inserts "Hello" to the current caret position. Actually, this is a
+// template for making your custom Action that inserts a specific text string.
+T( 'insert', {
+  text: 'Hello'
+});
