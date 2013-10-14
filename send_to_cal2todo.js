c312fd7cc54d41ca3d22bbf264a1c14cf31eb39e
succi0303 <succi0303@gmail.com>
2013-10-14 18:45:35 +0900

    tweak

--- send_to_cal2todo.js
+++ send_to_cal2todo.js
@@ -1,8 +1,10 @@
 // Cal2Todoで予定を登録する
 // 1行目をタイトル、2行目以降をノートとする
+var title = encodeURIComponent(T.line(1));
+var notes = encodeURIComponent(T.lines(2, null));
 T( 'urlScheme', {
-  url: 'cal2todo://?title=' + encodeURIComponent(T.line(1)) +
-    ',notes=' + encodeURIComponent(T.lines(2, null)) +
+  url: 'cal2todo://?title=' + title +
+    ',notes=' + notes +
     ',callback=textwell://',
   option: 'clearAll'
 });
